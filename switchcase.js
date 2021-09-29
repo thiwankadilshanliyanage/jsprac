e026fb293dc8547f4adf516056326d90285f9d59
thiwankadilshanliyanage <thiwankadilshanhub98@gmail.com>
2021-09-29 22:37:23 +0530

    first commit

---
+++ switchcase.js
@@ -0,0 +1,14 @@
+var empName = 'Kasun';
+var job = 'Doctor';
+
+switch (job.toLowerCase()) {
+    case 'teacher':
+        console.log(empName + ' is a teacher');
+        break;
+    case 'doctor':
+        console.log(empName + ' is a doctor');
+        break;
+    case 'engineer':
+        console.log(empName + ' is a engineer');
+        break;
+}
