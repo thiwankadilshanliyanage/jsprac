46e33277acb11542fa6d6d3a7af748bb71d51e30
thiwankadilshanliyanage <thiwankadilshanhub98@gmail.com>
2021-10-01 19:28:04 +0530

    first commit

--- function.js
+++ function.js
@@ -7,6 +7,10 @@ function addNumber(num1, num2) {
 //function calling
 addNumber(20, 32.5);
 
+
+
+
+
 //with return statement
 function subNumbers(num1, num2) {
     return num1 - num2;
@@ -20,3 +24,12 @@ function birthYear(bornyear) {
 }
 var bornIn = birthYear(1998);
 console.log('I am ' + bornIn + ' years old');
+
+function retiringYear(firstName, bornyear) {
+    var age = birthYear(bornyear);
+    var retire = 60 - age;
+    console.log(firstName + 'retiresin' + retire + 'years,in' + (bornyear + age + retire));
+
+}
+//calling
+retiringYear('Kamal', 1990);
