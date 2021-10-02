10c4d6e5eed0bd46a838a4a98251f85a3305751a
thiwankadilshanliyanage <thiwankadilshanhub98@gmail.com>
2021-10-02 23:04:06 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,8 @@
+//ways to define a array
+
+var student = ['Kamal', 'Nimal', 'Wimal', 'latha'];
+var mask = new Array(40, 50, 70, 56);
+
+//print array
+console.log(student);
+console.log(student[3]);
