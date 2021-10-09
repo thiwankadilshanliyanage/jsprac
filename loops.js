141acccea599692ff8a18693d9cd8bce69b77f6c
thiwankadilshanliyanage <thiwankadilshanhub98@gmail.com>
2021-10-09 23:43:49 +0530

    first commit

--- loops.js
+++ loops.js
@@ -1,8 +1,19 @@
 //for loop
+
+
+
 for (var i = 0; i < 10; i++) {
     console.log(i);
 }
 
 for (var i = 10; i >= 0; i--) {
     console.log(i);
+}
+
+
+
+//irerate array
+var ABC = ['A', 'B', 'C'];
+for (var i = 0; i < ABC.length; i++) {
+    console.log(ABC[i]);
 }
